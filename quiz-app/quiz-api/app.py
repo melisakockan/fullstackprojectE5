0afad1f025879f6057f85ef9dca43c91fc823da7
melisa.kockan@edu.esiee.fr <melisa.kockan@edu.esiee.fr>
2022-12-16 13:48:25 +0100

    partie 3 dockered

--- quiz-app/quiz-api/app.py
+++ quiz-app/quiz-api/app.py
@@ -3,6 +3,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/')
+def hello_world():
+	x = 'world'
+	return f"Hello, {x}, my name is Melisa !!!!"
+
 @app.route('/quiz-info', methods=['GET'])
 def GetQuizInfo():
 	return {"size": 0, "scores": []}, 200
