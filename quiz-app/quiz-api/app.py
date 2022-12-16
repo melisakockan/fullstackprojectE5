fea3fada0fb7bc287531e6619040a09eb33ddca1
melisa.kockan@edu.esiee.fr <melisa.kockan@edu.esiee.fr>
2022-12-16 14:50:14 +0100

    fin de partie 4

--- quiz-app/quiz-api/app.py
+++ quiz-app/quiz-api/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
+from jwt_utils import build_token
 app = Flask(__name__)
 CORS(app)
 
@@ -11,6 +12,17 @@ def hello_world():
 @app.route('/quiz-info', methods=['GET'])
 def GetQuizInfo():
 	return {"size": 0, "scores": []}, 200
+
+@app.route('/login', methods=['POST']) #ceci est un nouveau endpoint
+def Authentification():
+	payload = request.get_json()
+	if payload['password']=='flask2023':#Si le mot de passe est le bon
+		token_built=build_token()#On génère et retourne un token à partir du mdp
+		token={'token':token_built}
+		return token 
+	else:
+		return 'Unauthorized', 401 #Sinon, retourner une erreur 401
+
 
 if __name__ == "__main__":
     app.run()
