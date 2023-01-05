2df4472cc0e44c9761ba287c1279bc53d58bb928
SonOfSkywalker <raphael.canin@edu.esiee.fr>
2023-01-05 22:57:54 +0100

    Front About Page

--- quiz-app/quiz-ui/src/views/AboutView.vue
+++ quiz-app/quiz-ui/src/views/AboutView.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="about">
     <h1>About</h1>
+    <h2>
+      Créateurs : Raphaël & Melisa, E5AIC
+    </h2>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+
 </style>
