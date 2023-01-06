7e8a5b7bdb512926652586b19d31b078102b88a8
SonOfSkywalker <raphael.canin@edu.esiee.fr>
2023-01-06 15:01:04 +0100

    Front Clean Menu

--- quiz-app/quiz-ui/src/App.vue
+++ quiz-app/quiz-ui/src/App.vue
@@ -8,8 +8,6 @@ import { RouterLink, RouterView } from 'vue-router'
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/joueur">Joueur</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/admin">Admin</RouterLink>
 
       </nav>
--- quiz-app/quiz-ui/src/router/index.js
+++ quiz-app/quiz-ui/src/router/index.js
@@ -15,14 +15,6 @@ const router = createRouter({
       component: HomePage
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
       path: '/start-new-quiz-page', //clair et simple car visible par le user
       name: 'quiz',
       component: NewQuizPage
@@ -37,12 +29,6 @@ const router = createRouter({
       name: 'score',
       component: Score
     },
-    {
-      path: '/joueur',
-      name: 'joueur',
-      component: Joueur
-    },
-
     {
       path: '/admin',
       name: 'admin',
